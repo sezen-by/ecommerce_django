bc28165866c04632239480f8e7a08536bfff00e8
sezen-by <sezenmelisoktay.by@gmail.com>
2025-02-28 08:32:21 +0300

    27subat

--- store/models.py
+++ store/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'categories'
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
@@ -23,6 +26,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, null=True, blank=True, default='')   
     image = models.ImageField(upload_to='uploads/products/')
+    #Add Sale Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, max_digits=6, decimal_places=2) 
 
     def __str__(self):
         return self.name  
