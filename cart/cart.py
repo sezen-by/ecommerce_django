26944cff25b7458d412b7a3d87702485cc6ede00
sezen-by <sezenmelisoktay.by@gmail.com>
2025-03-03 16:50:09 +0300

    3mart

--- cart/cart.py
+++ cart/cart.py
@@ -15,3 +15,26 @@ class Cart():
 
 		# Make sure cart is available on all pages of site
 		self.cart = cart
+
+
+	def add(self, product):
+		# Get product id
+		product_id = str(product.id)
+
+		# If product is not in cart, add it
+		if product_id not in self.cart:
+			self.cart[product_id] = {'price': str(product.price)}
+
+		# Save
+		self.session.modified = True
+
+	def __len__(self):
+		return len(self.cart)
+
+	def get_prods(self):
+		# Get ids from carts
+		product_ids = self.cart.keys()
+		# Use ids to lookup products in database model
+		products = Product.objects.filter(id__in=product_ids)
+		# Return those looked up products
+		return products
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render, get_object_or_404
-#from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
 from django.contrib import messages
+from .cart import Cart
 
 def cart_summary(request):
-    return render(request, 'cart_summary.html')
+	# Get the cart
+	cart = Cart(request)
+	# Get the products
+	cart_products = cart.get_prods()
+	return render(request, 'cart_summary.html', {"cart_products": cart_products})
+
+def cart_add(request):
+	# Get the cart
+	cart = Cart(request)
+	# test for POST
+	if request.POST.get('action') == 'post':
+		# Get stuff
+		product_id = int(request.POST.get('product_id'))
+
+		# lookup product in DB
+		product = get_object_or_404(Product, id=product_id)
+		
+		# Save to session
+		cart.add(product=product)
+
+		# Get Cart Quantity
+		cart_quantity = cart.__len__()
+        
+		# Return resonse
+		# response = JsonResponse({'Product Name: ': product.name})
+		print(f"✅ Sepetteki toplam ürün: {cart_quantity}")
+		response = JsonResponse({'qty': cart_quantity})
+		messages.success(request, ("Product Added To Cart..."))
+		return response
