0c23307be5f7a0373ca8bac16ea9fd9fc0ffa638
sezen-by <sezenmelisoktay.by@gmail.com>
2025-03-04 16:53:29 +0300

    4mart

--- cart/cart.py
+++ cart/cart.py
@@ -1,40 +1,56 @@
 from store.models import Product
 
 class Cart():
-	def __init__(self, request):
-		self.session = request.session
-		# Get request
-		self.request = request
-		# Get the current session key if it exists
-		cart = self.session.get('session_key')
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('session_key')
 
-		# If the user is new, no session key!  Create one!
-		if 'session_key' not in request.session:
-			cart = self.session['session_key'] = {}
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+
+        self.cart = cart
+
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        product_qty = str(quantity)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': int(product_qty)}
+        else:
+            self.cart[product_id]['quantity'] += int(product_qty)
+            
+        self.session.modified = True
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+        
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        # Get cart
+        ourcart = self.cart
+        # Update Dictionary/cart
+        ourcart[product_id] = product_qty
+
+        self.session.modified = True
+	
+
+    def delete(self, product):
+        product_id = str(product)
+        # Delete from dictionary/cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
 
 
-		# Make sure cart is available on all pages of site
-		self.cart = cart
-
-
-	def add(self, product):
-		# Get product id
-		product_id = str(product.id)
-
-		# If product is not in cart, add it
-		if product_id not in self.cart:
-			self.cart[product_id] = {'price': str(product.price)}
-
-		# Save
-		self.session.modified = True
-
-	def __len__(self):
-		return len(self.cart)
-
-	def get_prods(self):
-		# Get ids from carts
-		product_ids = self.cart.keys()
-		# Use ids to lookup products in database model
-		products = Product.objects.filter(id__in=product_ids)
-		# Return those looked up products
-		return products
