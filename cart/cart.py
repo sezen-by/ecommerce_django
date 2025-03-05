7dcdba488fbb11a2a34dc1efe4d6b7d8627f7d76
sezen-by <sezenmelisoktay.by@gmail.com>
2025-03-05 11:52:23 +0300

    5ekim sepet kısmı sıkıntılı

--- cart/cart.py
+++ cart/cart.py
@@ -22,7 +22,9 @@ class Cart():
         self.session.modified = True
 
     def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values())
+        return sum(item.get('quantity', 0) if isinstance(item, dict) else 0 for item in self.cart.values())
+
+
 
     def get_prods(self):
         product_ids = self.cart.keys()
@@ -34,7 +36,7 @@ class Cart():
         return quantities
         
     def update(self, product, quantity):
-        product_id = str(product)
+        product_id = str(product.id)
         product_qty = int(quantity)
 
         # Get cart
@@ -43,14 +45,18 @@ class Cart():
         ourcart[product_id] = product_qty
 
         self.session.modified = True
+
+        thing = self.cart
+        return thing
+    
 	
 
     def delete(self, product):
-        product_id = str(product)
-        # Delete from dictionary/cart
-        if product_id in self.cart:
-            del self.cart[product_id]
+        pass
+        #product_id = str(product)
+            # Delete from dictionary/cart
+        #if product_id in self.cart:
+            #del self.cart[product_id]
 
-        self.session.modified = True
-
+        #self.session.modified = True
 
--- cart/views.py
+++ cart/views.py
@@ -15,7 +15,7 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_qty = int(request.POST.get('product_qty', 1))  # Varsayılan olarak 1
-        product = get_object_or_404(Product, id=product_id)
+        product = get_object_or_404(Product, id=product_id) #Product modelinden product_id ile ilgili ürün alınır.
         cart.add(product=product, quantity=product_qty)
         cart_quantity = cart.__len__()
         response = JsonResponse({'qty': cart_quantity})
@@ -34,19 +34,20 @@ def cart_update(request):
 
 		response = JsonResponse({'qty':product_qty})
 		#return redirect('cart_summary')
-		messages.success(request, ("Your Cart Has Been Updated..."))
+
 		return response
     
 def cart_delete(request):
-	cart = Cart(request)
-	if request.POST.get('action') == 'post':
-		# Get stuff
-		product_id = int(request.POST.get('product_id'))
-		# Call delete Function in Cart
-		cart.delete(product=product_id)
+    pass
+	# cart = Cart(request)
+	# if request.POST.get('action') == 'post':
+	# 	# Get stuff
+	# 	product_id = int(request.POST.get('product_id'))
+	# 	# Call delete Function in Cart
+	# 	cart.delete(product=product_id)
 
-		response = JsonResponse({'product':product_id})
-		#return redirect('cart_summary')
-		messages.success(request, ("Item Deleted From Shopping Cart..."))
-		return response
+	# 	response = JsonResponse({'product':product_id})
+	# 	#return redirect('cart_summary')
+	# 	messages.success(request, ("Item Deleted From Shopping Cart..."))
+	# 	return response 
 
